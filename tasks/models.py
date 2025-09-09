3726cf7f3cddc61f3793d7773901b720e8b8acf5
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-09-09 16:33:48 -0300

    Criei o Model Task, fiz as migrações e registrei no admin.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(verbose_name='title', max_length=50)
+    description = models.TextField(verbose_name='description')
+    completed = models.BooleanField(verbose_name='completed')
+    created_at = models.DateTimeField(
+        verbose_name='created at', auto_now=False, auto_now_add=False, blank=True)
