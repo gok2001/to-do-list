cf132886e46b966afa797cd9f8bd367863eb5ea6
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-09-11 15:52:12 -0300

    Adicionei a url no tasks/urls.py e incluí no project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('tasks/', include('tasks.urls', namespace='tasks')),
     path('', include('tasks.urls')),
     path('admin/', admin.site.urls),
 ]
